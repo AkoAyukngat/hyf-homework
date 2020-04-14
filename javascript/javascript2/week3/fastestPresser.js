343acd6b2cd23cc7605cd86a521862410c07bee9
AkoAyukngat <johnako64@gmail.com>
2020-04-14 23:01:29 +0200

    JS2 WEEK3 HomeWork

---
+++ javascript/javascript2/week3/fastestPresser.js
@@ -0,0 +1,58 @@
+const timerInput = document.querySelector("#inputButton");
+const startGame = document.querySelector("#button1");
+const playerS = document.querySelector(".PlyS");
+const playerL = document.querySelector(".PlyL");
+const result = document.querySelector(".Result");
+const startAgain = document.querySelector(".Restart");
+
+let counterS = 0;
+let counterL = 0;
+
+//#2 Set a timeout for the time specified by the user:
+
+function playGame() {
+  setTimeout(() => {
+    if (!timerInput.value) {
+      alert("Please enter gaming time");
+    } else {
+      alert("Game Over!");
+
+      const playerResult = () => {
+        if (counterS > counterL) {
+          result.innerHTML = "Player S won!";
+        }
+        if (counterL > counterS) {
+          result.innerHTML = "Player L won!";
+        }
+        if (counterS === counterL) {
+          result.innerHTML = "It was a draw";
+        }
+      };
+      playerResult();
+    }
+  }, timerInput.value * 1000);
+}
+//#3 Create an event listener so you can call a function when any key is pressed:
+startGame.addEventListener("click", playGame);
+
+//#4 Keep a counter for how many times l and s was pressed:
+function keyPressCounter(keyPress) {
+  if (keyPress.key === "l") {
+    counterL++;
+    playerL.innerHTML = `${counterL}`;
+    console.log(`${counterL}`);
+  }
+  if (keyPress.key === "s") {
+    counterS++;
+    playerS.innerHTML = `${counterS}`;
+    console.log(`${counterS}`);
+  }
+}
+document.addEventListener("keypress", keyPressCounter);
+
+//#5 Start a new game functionality:
+
+function startAllOver() {
+  location.reload();
+}
+startAgain.addEventListener("click", startAllOver);
---
+++ javascript/javascript2/week3/fxnAsVariable.js
@@ -0,0 +1,41 @@
+//#1 Create an array with 3 items,iterate through the array and call all the functions:
+
+const arrayOfFunctions = [
+    function oldBooks() {
+        console.log('Jane Eyre');
+    },
+    function spices() {
+        console.log('Turmeric');  
+    },
+    function bread() {
+        console.log('Walnut Bread');
+    }
+];
+//arrayOfFunctions.forEach(func => func());
+arrayOfFunctions.forEach(function (func) {
+    return func();
+})
+
+//#2 Create a function as a const and try creating a function normally:
+
+const todaysLunch = () => {
+    console.log('Stewed Cow Livers');
+};
+
+function sunBathingHour() {
+    console.log('13:00')
+}
+
+todaysLunch();
+sunBathingHour();
+
+//#3 Create an object that has a key whose value is a function:
+
+const omega3Supplements = {
+    ingredients1: 'fish oil',
+    ingredients2: 'Water',
+    ingredients3: function vitamines() {
+        console.log ('vit: A,D,E & K');
+    }
+};
+(omega3Supplements.ingredients3());
