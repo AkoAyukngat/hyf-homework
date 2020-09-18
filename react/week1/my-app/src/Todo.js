bdb79b0d525114ea1240495f98d5945086a89aa8
AkoAyukngat <johnako64@gmail.com>
2020-09-18 11:41:19 +0200

    made some modifications

--- react/week1/my-app/src/Todo.js
+++ react/week1/my-app/src/Todo.js
@@ -1,56 +1,57 @@
 import React, { Component } from "react";
 import "./Todo.css";
 
+function DeadLine(props) {
+  const styleListdate = {
+    color: "green",
+    width: "390px",
+    margin: "20px",
+    fontFamily: "Arial",
+    listStyleType: "none",
+  };
+
+  return <span style={styleListdate}>{props.children}</span>;
+}
+
 export class Todo extends Component {
   render() {
-    const styleListOne = {
-      borderStyle: "solid",
-      width: "390px",
-      color: "red",
-      margin: "20px",
-      textColor: "blue",
-      padding: "10px",
-      fontFamily: "Arial",
-      listStyleType: "none",
-    };
-    const styleListTwo = {
-      borderStyle: "solid",
-      width: "390px",
-      color: "violet",
-      listStyleType: "none",
-      textColor: "blue",
-      margin: "20px",
-      padding: "10px",
-      fontFamily: "Arial",
-    };
-    const styleListThree = {
+    
+    const allListStyles = {
       borderStyle: "solid",
       width: "490px",
       margin: "20px",
       listStyleType: "none",
       color: "DodgerBlue",
-      textColor: "blue",
       padding: "10px",
       fontFamily: "Arial",
     };
 
+    const todoList = [
+      {
+        description: "Book seat at Royal Opera",
+        deadline: "Tuesday, August 4 2020",
+      },
+      {
+        description: "Register for moms",
+        deadline: "Friday the 16th 06, 2020",
+      },
+      {
+        description: "Build baby crip,wagon and wash basin",
+        deadline: "Monday September 31 2020",
+      },
+    ];
+
     return (
-      <ul>
-        <li style={styleListOne}>
-          Book a seat at Royal Opera, Tuesday, August 4 2020
-        </li>
-        <li style={styleListTwo}>Register for moms, Monday August 17 2020 </li>
-        <li style={styleListThree}>
-          Build baby crip,wagon and wash basin,Monday September 31 2020
-        </li>
-      </ul>
+    
+      <div>
+        {todoList.map((todo) => (
+          <li style={allListStyles}>
+            {todo.description}, <DeadLine>{todo.deadline}</DeadLine>
+          </li>
+        ))}
+      </div>
     );
   }
 }
 
-// class desc {
-//   constructor(color) {
-//     this.color = color;
-//   }
-// }
-// new desc("orange");
+
